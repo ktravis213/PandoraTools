d7c98116f5d8075f9848ac08befed90a3c894a4b
Katherine Travis <ktravis@fas.harvard.edu>
2025-06-05 16:33:43 -0400

    Edit comments

--- GetPandorafromRSIG.py
+++ GetPandorafromRSIG.py
@@ -17,18 +17,20 @@ from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 
+# This should cover CONUS Pandora sites
 lon1, lon2 = -126, -66
 lat1, lat2 = 24,50
  
 # Date range
 start_date = datetime.strptime('2024-06-01', "%Y-%m-%d")
-end_date = datetime.strptime('2024-06-02', "%Y-%m-%d")
+end_date = datetime.strptime('2024-08-31', "%Y-%m-%d")
  
 # Generate list of dates
 date_list = [(start_date + timedelta(days=x)).strftime("%Y-%m-%d")
               for x in range((end_date - start_date).days + 1)]
  
-# The API call doesn't seem to like doing large bounding boxes + large dates. Thus the reading day by day .
+# The API call doesn't seem to like doing large bounding boxes + large dates. Thus the reading day by day.
+# RSIG only allows you to pull one variable from Pandora at a time. If another variable is needed in addition to the column product and quality flag, it can be added in the function below.
 def fetch_pandora_data(date_str, version, column_product, qf_product):
     try:
         base_url = "https://ofmpub.epa.gov/rsig/rsigserver?SERVICE=wcs&VERSION=1.0.0&REQUEST=GetCoverage"
